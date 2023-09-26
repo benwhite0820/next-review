ea9ab7c95e7ae92f0ce4ea453c4f61a74586c2c1
benwhite0820 <dragon7230@gmail.com>
2023-09-26 12:31:54 +0800

    style card component

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,4 +1,5 @@
 import Heading from '@/components/heading.component';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -6,12 +7,30 @@ const Reviews = () => {
   return (
     <>
       <Heading>Review Area</Heading>
-      <ul>
-        <li>
-          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+      <ul className="flex flex-col gap-3">
+        <li className="bg-white border rounded shadow w-80 transition-all hover:shadow-xl">
+          <Link href="/reviews/hollow-knight">
+            <Image
+              src="/images/hollow-knight.jpg"
+              alt="hollow knight image"
+              width="320"
+              height="180"
+              className="rounded-t"
+            />
+            <h2 className="py-1 text-center">Hollow Knight</h2>
+          </Link>
         </li>
-        <li>
-          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        <li className="bg-white border rounded shadow w-80 transition-all hover:shadow-xl">
+          <Link href="/reviews/stardew-valley">
+            <Image
+              src="/images/stardew-valley.jpg"
+              alt="stardew valley image"
+              width="320"
+              height="180"
+              className="rounded-t"
+            />
+            <h2 className="py-1 text-center">Stardew Valley</h2>
+          </Link>
         </li>
       </ul>
     </>
--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,10 +1,18 @@
 import Heading from '@/components/heading.component';
+import Image from 'next/image';
 import React from 'react';
 
 const StardewVallyPage = () => {
   return (
     <>
       <Heading>StardewVallyPage</Heading>
+      <Image
+        src="/images/stardew-valley.jpg"
+        alt="stardew valley image"
+        width="640"
+        height="360"
+        className="mb-2 rounded"
+      />
       <div>This will be the review for Stardew Valley</div>
     </>
   );
