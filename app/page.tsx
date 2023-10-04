818177f93169a66ff5385fdac9f21171192006a4
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2023-10-04 23:37:01 +0800

    add new image to stardew valley compoent

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
 import Heading from '@/components/heading.component';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Home = () => {
   return (
     <>
       <Heading>Indie Gamer</Heading>
-      <p>only the best indie games, reviewed for you</p>
+      <p className="pb-3">only the best indie games, reviewed for you</p>
+      <div className="bg-white border rounded shadow transition-all hover:shadow-xl w-80 sm:w-full">
+        <Link
+          className="flex flex-col sm:flex-row"
+          href="/reviews/stardew-valley"
+        >
+          <Image
+            src="/images/stardew-valley.jpg"
+            alt="stardew valley image"
+            width="320"
+            height="180"
+            className="rounded-t sm:rounded-l sm:rounded-r-none"
+          />
+          <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
+            Stardew Valley
+          </h2>
+        </Link>
+      </div>
     </>
   );
 };
