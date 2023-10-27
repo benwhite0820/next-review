3138f4c01f148ee3ccbc08f465cad416abb9065d
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2023-10-27 13:10:39 +0800

    add getSerchReviews fn and modify getAllReviews fn

--- lib/review.ts
+++ lib/review.ts
@@ -1,7 +1,13 @@
+import 'server-only';
 import { sanitize } from 'isomorphic-dompurify';
 import { marked } from 'marked';
 import { Review, StrapiReviewPaginationType } from '@/types/review.types';
 import { fetchReviewData, formattedReview } from '@/utils/fetchReviewHelpers';
+
+type GetReviewArgs = {
+  pageSize?: number;
+  page?: number;
+};
 
 export const getSlugs = async () => {
   const { data } = await fetchReviewData({
@@ -40,12 +46,9 @@ export async function getReview(slugParam: string): Promise<Review | null> {
 }
 
 export async function getAllReviews({
-  pageSize = 6,
+  pageSize,
   page,
-}: {
-  pageSize?: number;
-  page?: number;
-}): Promise<{
+}: GetReviewArgs): Promise<{
   data: Omit<Review, 'html'>[];
   meta: StrapiReviewPaginationType;
 }> {
@@ -59,4 +62,21 @@ export async function getAllReviews({
   });
 
   return { data: data.map((item) => formattedReview(item)), meta };
+}
+
+export async function getSearchReviews(
+  query: string
+): Promise<{ slug: string; title: string }[]> {
+  const { data } = await fetchReviewData({
+    filterFiled: 'title',
+    filterParams: query,
+    filterOperator: '$containsi',
+    searchField: ['slug', 'title'],
+    sortField: ['title'],
+    pagination: {
+      pageSize: 5,
+    },
+  });
+
+  return data.map(({ attributes: { slug, title } }) => ({ slug, title }));
 }
